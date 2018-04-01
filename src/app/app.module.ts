ef018f84ee3635d3ec921f51acc35ecade56dff7
Arne Siemens <arsiemens@gmail.com>
2018-04-01 22:22:23 +0200

    Basic inline route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 
+@Component({
+  template: `
+    I'm a home component
+  `
+})
+export class HomeComponent {}
+
+const routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
