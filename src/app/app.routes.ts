1f5d177559497054b678d739f994f9bae68f5937
Arne Siemens <arsiemens@gmail.com>
2018-04-02 13:18:13 +0200

    Bug note added to lazyload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import ContactsModule from './contacts/contacts.module';
 import HomeModule from './home/home.module';
 import DashModule from './dash/dash.module';
 
-
+// Consider this: https://github.com/angular/angular-cli/issues/9825
 const routes = [
   { path: '', loadChildren: () => HomeModule },
   { path: 'contacts', loadChildren: () => ContactsModule },
